5afeb58e6bb5654892a243569d8aa5ed2998a855
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-21 22:01:35 +0530

    ,

---
+++ day11/rotatestirng/code.java
@@ -0,0 +1,9 @@
+package day11.rotatestirng;
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())return false;
+        return(s+s).contains(goal);
+        
+    }
+}

6c07ee5e7855e49a9c5a65403ca78977b353475e
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-06 22:24:40 +0530

    3110

--- day22/3110/code.java
+++ day22/3110/code.java
@@ -1,5 +1,12 @@
-package day22.3110;
-
-public class code {
-    
+class Solution {
+    public int scoreOfString(String s) {
+        int minus=0,sum=0;
+        for(int i=1;i<s.length();i++){
+            int temp1=(int)s.charAt(i);
+            int temp2=(int)s.charAt(i-1);
+            sum+=Math.abs(temp1-temp2);
+        }
+        return sum;
+        
+    }
 }

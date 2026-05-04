810a21f108671a476a11f072a0f38581323355a3
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2026-05-04 19:33:55 +0530

    one

---
+++ Recursion/subwithone.java
@@ -0,0 +1,25 @@
+public class Subsequence with sum one time {
+     public static boolean ss(int[] arr,int i,ArrayList<Integer>current,int s,int sum){
+     if(i==arr.length){
+            if(s==sum){
+            System.out.print(current);
+            return true;
+            }
+            else return false;
+        }
+
+        current.add(arr[i]);
+        s+=arr[i];
+      if ( ss(arr,i+1,current,s,sum)==true)return true;
+        current.remove(current.size()-1);
+        s-=arr[i];
+       if ( ss(arr,i+1,current,s,sum)==true)return true;
+       return false;
+    }
+    public static void main(String[] args) {
+      int[] arr={3,1,2};
+      int sum=3;
+       ArrayList<Integer> current = new ArrayList<>();
+      ss(arr,0,current,0,sum);
+    }
+}
